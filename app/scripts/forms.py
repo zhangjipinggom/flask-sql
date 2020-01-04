09e18a4e55241444d8f0e475f286565ee0dd8c15
zhangjipinggom <34130629+zhangjipinggom@users.noreply.github.com>
2020-01-04 09:51:42 +0800

    Update forms.py

--- app/scripts/forms.py
+++ app/scripts/forms.py
@@ -4,7 +4,7 @@ from wtforms import Form, StringField, validators
 
 
 class LoginForm(Form):
-    username = StringField('Username:', validators=[validators.required(), validators.Length(min=8, max=30),
-                                                    validators.Regexp(r'ilovebme', 0, 'not allowed name')])
+    username = StringField('Username:', validators=[validators.required(), validators.Length(min=8, max=30)
+                                                    ])
     password = StringField('Password:', validators=[validators.required(), validators.Length(min=1, max=30)])
     email = StringField('Email:', validators=[validators.optional(), validators.Length(min=0, max=50)])
